01c81e9e99ecbdeb82d46428608a390cafe5173f
Lenka <Lenka@LenovoPC>
2016-03-29 22:45:55 +0200

    konecne hotove order testy, ale neprechadza ani jeden :D

--- band_v2/BandsProject/src/test/java/cz/muni/fi/pv168/bandsproject/OrderManagerImplTest.java
+++ band_v2/BandsProject/src/test/java/cz/muni/fi/pv168/bandsproject/OrderManagerImplTest.java
@@ -14,8 +14,10 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Objects;
 import org.apache.derby.jdbc.EmbeddedDataSource;
 
@@ -138,7 +140,7 @@ public class OrderManagerImplTest {
         Customer customer = newCustomer("Pepa", "666 123 456", "Kartouzska 69");
         managerCustomer.createCustomer(customer);
         
-        Customer customer2 = newCustomer("Lenka si to cte :O", "666 555 456", "Kartouzska 69");
+        Customer customer2 = newCustomer("Lenka si to cte :O", "Samozrejme :P 666", "Kartouzska 69");
         managerCustomer.createCustomer(customer2);
         
         
@@ -273,17 +275,178 @@ public class OrderManagerImplTest {
 
     @Test
     public void testFindAllOrders() throws Exception {
-
+        SimpleDateFormat sfd = new SimpleDateFormat("dd.mm.yyyy");
+        
+        Collection<Order> allCustomers = managerOrder.findAllOrders();
+        
+        assertTrue(allCustomers.isEmpty());
+        
+        Customer cus1 = newCustomer("Igor Kosik", "+421 944 326 548", "Kernova 9");
+        managerCustomer.createCustomer(cus1);
+        List<Style> styles = new ArrayList<>();
+        styles.add(Style.blues);
+        styles.add(Style.reggae);
+        Band band1 = newBand("A ty citas toto?", styles, Region.karlovarsky, 156.23, 6.1);
+        managerBand.createBand(band1);
+        
+        Order order1 = newOrder(band1, cus1, "01.12.2017", Region.kralovohradecky, 4);
+        managerOrder.createOrder(order1);
+        
+        Customer cus2 = newCustomer("Frantisek Smutny", "236 155 885", "Most SNP 4.stlp");
+        managerCustomer.createCustomer(cus2);
+        Band band2 = newBand("HEX", styles, Region.slovensko, 421.00, 5.5);
+        managerBand.createBand(band2);
+        
+        Order order2 = newOrder(band2, cus2, "05.06.2016", Region.praha, 3);
+        managerOrder.createOrder(order2);
+        
+        List<Order> sample = new ArrayList<>();
+        List<Order> actual = new ArrayList<>();
+        
+        sample.add(order1);
+        sample.add(order2);
+        
+        allCustomers = managerOrder.findAllOrders();
+        actual.addAll(allCustomers);
+        
+        Collections.sort(sample, idComparator);
+        Collections.sort(actual, idComparator);
+        
+        assertDeepEquals(sample, actual);
+        
     }
 
     @Test
     public void testFindOrdersForBand() throws Exception {
+        Customer customer1 = newCustomer("Tomas Oravec","012 345 789", "Brezno 123");
+        managerCustomer.createCustomer(customer1);
+        List<Style> styles = new ArrayList<>();
+        styles.add(Style.dnb);
+        styles.add(Style.classical);
+        Band band1 = newBand("Ugly band", styles, Region.liberecky, 569.36, 1.1);
+        managerBand.createBand(band1);
 
+        Order order1 = newOrder(band1, customer1, "31.02.2019", Region.jihocesky, 6);
+        managerOrder.createOrder(order1);
+
+        Customer customer2 = newCustomer("Ondrej Brezovec", "569 123 789", "Zilina 1020");
+        managerCustomer.createCustomer(customer2);
+        Band band2 = newBand("Nice band", styles, Region.karlovarsky, 222.66, 3.3);
+        managerBand.createBand(band2);
+
+        Order order2 = newOrder(band2, customer2, "23.06.2017", Region.praha, 3);
+        managerOrder.createOrder(order2);
+
+        Customer customer3 = newCustomer("Mario Hubrak", "566 222 111", "Lucenec 183");
+        managerCustomer.createCustomer(customer3);
+
+        order1=managerOrder.findOrderById(order1.getId());
+
+        Order order3 = newOrder(band1, customer3, "22.08.2016", Region.ustecky, 5);
+        managerOrder.createOrder(order3);
+
+        List<Order> sample= new ArrayList<>();
+        List<Order> actual= new ArrayList<>();
+
+        sample.add(order1);
+        sample.add(order3);
+
+        Collection<Order> ordersForBand = managerOrder.findOrdersForBand(band1);
+        actual.addAll(ordersForBand);
+
+        Collections.sort(sample,idComparator);
+        Collections.sort(actual, idComparator);
+
+        assertDeepEquals(sample,actual);
+
+        // band don't have order
+        Band band3 = newBand("Lonely band", styles, Region.olomoucky, 55.33, 2.3);
+        managerBand.createBand(band3);
+        ordersForBand = managerOrder.findOrdersForBand(band3);
+        assertTrue(ordersForBand.isEmpty());
+
+        //consistency
+        Order getOrder = managerOrder.findOrderById(order2.getId());
+        assertDeepEquals(order2,getOrder);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindOrdersForBandWithWrongArgument() throws Exception {
+        Collection<Order> ordersForBand = new ArrayList<>();
+
+        ordersForBand=managerOrder.findOrdersForBand(null);
+        
+        List<Style> styles = new ArrayList<>();
+        styles.add(Style.folk);
+        styles.add(Style.disco);
+        Band band1 = newBand("I drift off", styles, Region.stredocesky, 222.33, 6.6);
+        
+        ordersForBand = managerOrder.findOrdersForBand(band1);
     }
 
     @Test
     public void testFindOrdersForCustomer() throws Exception {
+        Customer customer1 = newCustomer("Tomas Oravec","+421 944 222 222", "Brezno 123");
+        managerCustomer.createCustomer(customer1);
+        
+        List<Style> styles = new ArrayList<>();
+        styles.add(Style.pop);
+        styles.add(Style.jazz);
+        Band band1 = newBand("Nemozem povedat", styles, Region.slovensko, 123.45, 6.7);
+        managerBand.createBand(band1);
 
+        Order order1 = newOrder(band1, customer1, "11.04.2016", Region.plzensky, 5);
+        managerOrder.createOrder(order1);
+
+        Customer customer2 = newCustomer("Ondrej Brezovec","+421 922 222 222", "Zilina 1020");
+        managerCustomer.createCustomer(customer2);
+        Band band2 = newBand("No name", styles, Region.slovensko, 100.22, 4.6);
+        managerBand.createBand(band2);
+
+        Order order2 = newOrder(band2, customer2, "23.07.2017", Region.liberecky, 7);
+        managerOrder.createOrder(order2);
+
+        Band band3 = newBand("AC/DC", styles, Region.moravskosliezsky, 45.26, 8.9);
+        managerBand.createBand(band3);
+
+        Order order3 = newOrder(band3, customer1, "15.03.2016", Region.pardubicky, 4);
+        managerOrder.createOrder(order3);
+
+        List<Order> sample= new ArrayList<>();
+        List<Order> actual= new ArrayList<>();
+
+        sample.add(order1);
+        sample.add(order3);
+
+        Collection<Order> ordersForCustomer=managerOrder.findOrdersForCustomer(customer1);
+        actual.addAll(ordersForCustomer);
+
+        Collections.sort(sample,idComparator);
+        Collections.sort(actual, idComparator);
+
+        assertDeepEquals(sample,actual);
+
+        // test with customer who doesn't have order
+        Customer customer3 = newCustomer("Andrej Pincik", "568 789 123", "Brezovec 10");
+        managerCustomer.createCustomer(customer3);
+        ordersForCustomer=managerOrder.findOrdersForCustomer(customer3);
+        assertTrue(ordersForCustomer.isEmpty());
+
+        //consistency
+        Order getOrder = managerOrder.findOrderById(order2.getId());
+        assertDeepEquals(order2,getOrder);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindOrdersForCustomerWithWrongArgument() throws Exception {
+        Collection<Order> ordersForCustomer = new ArrayList<>();    
+        
+        ordersForCustomer = managerOrder.findOrdersForCustomer(null);
+            
+        //customer isn't in DB
+        Customer customer2 = newCustomer("Tomas Mician","065 482 266", "Cadca 123");
+        ordersForCustomer = managerOrder.findOrdersForCustomer(customer2); 
     }
     
     /**
@@ -350,4 +513,20 @@ public class OrderManagerImplTest {
         
         assertFalse(res);
     }
+
+    private void assertDeepEquals(List<Order> sample, List<Order> actual) {
+        for(int i = 0; i < sample.size(); i++) {
+            Order first = sample.get(i);
+            Order second = actual.get(i);
+            assertDeepEquals(first, second);
+        }
+    }
+    
+    private static Comparator<Order> idComparator = new Comparator<Order>() {
+
+        @Override
+        public int compare(Order o1, Order o2) {
+            return o1.getId().compareTo(o2.getId());
+        }
+    };
 }
